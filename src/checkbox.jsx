811d58d03ddf8a91d57e9a648648728710abdc68
David Suárez <dsuarezv@gmail.com>
2018-01-15 17:02:27 +0000

    Improvements to the checkbox

--- src/checkbox.jsx
+++ src/checkbox.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Checkbox from 'material-ui/Checkbox'
 import * as Colors from 'material-ui/colors'
 import {FieldType} from 'simple-react-form'
+import FormControlLabel from 'material-ui/Form/FormControlLabel';
 
 const propTypes = {
   ...FieldType.propTypes
@@ -15,13 +16,18 @@ export default class CheckboxComponent extends React.Component {
 
   render () {
     return (
-      <div style={{ paddingTop: 10, paddingBottom: 10 }}>
-        <Checkbox
-          disabled={this.props.disabled}
-          checked={typeof this.props.value !== 'undefined' ? this.props.value : false }
-          onChange={() => this.props.onChange(!this.props.value)}
-          {...this.props.passProps}
-        />
+      <div>
+      <FormControlLabel 
+        control={
+          <Checkbox
+            disabled={this.props.disabled}
+            checked={typeof this.props.value !== 'undefined' ? this.props.value : false }
+            onChange={() => this.props.onChange(!this.props.value)}
+            {...this.props.passProps}
+          />
+        }
+        label={this.props.label} >
+        </FormControlLabel>
         <span style={{ color: Colors.red[500] }}>{this.props.errorMessage}</span>
       </div>
     )
